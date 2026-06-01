05867f80b5eba20c35e38924f3145e83cfdb8d21
LK Cheung <lkc@LKs-MacBook-Pro.local>
2026-06-01 16:57:10 +0100

    feat(cdk): JIRA123 - added serverless, hello-world lambda and api gateway

--- cdk_hello_world/cdk_hello_world_stack.py
+++ cdk_hello_world/cdk_hello_world_stack.py
@@ -1,7 +1,9 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    # Import Lambda L2 construct
+    aws_lambda as _lambda,
+    aws_apigateway as apigateway,
 )
 from constructs import Construct
 
@@ -10,10 +12,23 @@ class CdkHelloWorldStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Define the Lambda function resource
+        hello_world_function = _lambda.Function(
+            self,
+            "HelloWorldFunction",
+            runtime = _lambda.Runtime.NODEJS_20_X, # Choose any supported Node.js runtime
+            code = _lambda.Code.from_asset("lambda"), # Points to the lambda directory
+            handler = "hello.handler", # Points to the 'hello' file in the lambda directory
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkHelloWorldQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Define the API Gateway resource
+        api = apigateway.LambdaRestApi(
+            self,
+            "HelloWorldApi",
+            handler = hello_world_function,
+            proxy = False,
+        )
+
+        # Define the '/hello' resource with a GET method
+        hello_resource = api.root.add_resource("hello")
+        hello_resource.add_method("GET")
