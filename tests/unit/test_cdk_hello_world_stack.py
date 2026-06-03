aeabb2c5048786396b5a326bee3c5c137345ab88
hustlekat <hustlekat@example.com>
2026-06-03 13:59:26 +0100

    feat(gha): JIRA-123 - adding ci/cd

--- tests/unit/test_cdk_hello_world_stack.py
+++ tests/unit/test_cdk_hello_world_stack.py
@@ -5,11 +5,11 @@ from cdk_hello_world.cdk_hello_world_stack import CdkHelloWorldStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in cdk_hello_world/cdk_hello_world_stack.py
-def test_sqs_queue_created():
+def test_lambda_created():
     app = core.App()
     stack = CdkHelloWorldStack(app, "cdk-hello-world")
     template = assertions.Template.from_stack(stack)
 
     template.has_resource_properties("AWS::Lambda::Function", {
-        "timeout": 3
+        "runtime": "nodejs20.x"
     })
